20acb06ec7b35dc275c0006a636fa8546a3c964c
Rob Marsh <rob.marsh@wearesauce.io>
2020-12-21 12:15:56 +0000

    add unit test for vtl templating

--- __tests__/steps/given.js
+++ __tests__/steps/given.js
@@ -1,11 +1,13 @@
-const { LexModelBuildingService } = require('aws-sdk');
-
-const chance = require('chance').Chance();
+const chance = require("chance").Chance();
+const velocityUtil = require("amplify-appsync-simulator/lib/velocity/util");
 
 const a_random_user = () => {
-  const firstName = chance.first({ nationality: 'en' });
-  const lastName = chance.first({ nationality: 'en' });
-  const suffix = chance.string({ length: 4, pool: 'abcdefghijklmnopqrstuvwxyz' });
+  const firstName = chance.first({ nationality: "en" });
+  const lastName = chance.first({ nationality: "en" });
+  const suffix = chance.string({
+    length: 4,
+    pool: "abcdefghijklmnopqrstuvwxyz",
+  });
   const name = `${firstName} ${lastName} ${suffix}`;
   const password = chance.string({ length: 8 });
   const email = `${firstName}-${lastName}-${suffix}@appsyncmasterclass.com`;
@@ -17,6 +19,24 @@ const a_random_user = () => {
   };
 };
 
+const an_appsync_context = (identity, args) => {
+  const util = velocityUtil.create([], new Date(), Object());
+  const context = {
+    identity,
+    args,
+    arguments: args,
+  };
+
+  // appsync has a lot of different aliases
+  return {
+    context,
+    ctx: context,
+    util,
+    utils: util,
+  };
+};
+
 module.exports = {
   a_random_user,
+  an_appsync_context,
 };
--- __tests__/steps/when.js
+++ __tests__/steps/when.js
@@ -1,5 +1,8 @@
 require("dotenv").config();
 const AWS = require("aws-sdk");
+const fs = require("fs");
+const velocityMapper = require("amplify-appsync-simulator/lib/velocity/value-mapper/mapper");
+const velocityTemplate = require("amplify-velocity-template");
 
 const we_invoke_confirmUserSignup = async (username, name, email) => {
   const handler = require("../../functions/confirm-user-signup").handler;
@@ -60,7 +63,21 @@ const a_user_signs_up = async (name, email, password) => {
   };
 };
 
+// rarely need to unit tests this template mapping but useful if
+// writing complex vtl template and want to verify before deploying
+const we_invoke_an_appsync_template = (templatePath, context) => {
+  const template = fs.readFileSync(templatePath, { encoding: "utf-8" });
+  const ast = velocityTemplate.parse(template);
+  const compiler = new velocityTemplate.Compile(ast, {
+    valueMapper: velocityMapper.map,
+    escape: false,
+  });
+
+  return JSON.parse(compiler.render(context));
+};
+
 module.exports = {
   we_invoke_confirmUserSignup,
   a_user_signs_up,
+  we_invoke_an_appsync_template,
 };
