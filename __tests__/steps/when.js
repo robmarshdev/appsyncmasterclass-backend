b63ebfabea4ba81308c7653bbbbcc89ff1305d29
Rob Marsh <rob.marsh@wearesauce.io>
2020-12-21 10:41:43 +0000

    add first e2e test

--- __tests__/steps/when.js
+++ __tests__/steps/when.js
@@ -1,30 +1,66 @@
-require('dotenv').config();
+require("dotenv").config();
+const AWS = require("aws-sdk");
 
 const we_invoke_confirmUserSignup = async (username, name, email) => {
-  const handler = require('../../functions/confirm-user-signup').handler;
+  const handler = require("../../functions/confirm-user-signup").handler;
 
   const context = {};
   const event = {
-    "version": "1",
-    "region": process.env.AWS_REGION,
-    "userPoolId": process.env.COGNITO_USER_POOL_ID,
-    "userName": username,
-    "triggerSource": "PostConfirmation_ConfirmSignUp",
-    "request": {
-      "userAttributes": {
-        "sub": username,
+    version: "1",
+    region: process.env.AWS_REGION,
+    userPoolId: process.env.COGNITO_USER_POOL_ID,
+    userName: username,
+    triggerSource: "PostConfirmation_ConfirmSignUp",
+    request: {
+      userAttributes: {
+        sub: username,
         "cognito:email_alias": email,
         "cognito:user_status": "CONFIRMED",
-        "email_verified": "false",
-        "name": name,
-        "email": email,
-      }
-    }
+        email_verified: "false",
+        name: name,
+        email: email,
+      },
+    },
   };
 
   await handler(event, context);
 };
 
+const a_user_signs_up = async (name, email, password) => {
+  const cognito = new AWS.CognitoIdentityServiceProvider();
+
+  const userPoolId = process.env.COGNITO_USER_POOL_ID;
+  const clientId = process.env.WEB_COGNITO_USER_POOL_CLIENT_ID;
+
+  const response = await cognito
+    .signUp({
+      ClientId: clientId,
+      Username: email,
+      Password: password,
+      UserAttributes: [{ Name: "name", Value: name }],
+    })
+    .promise();
+
+  const username = response.UserSub;
+  console.log(`[${email} - user has signed up ${username}]`);
+
+  await cognito
+    .adminConfirmSignUp({
+      UserPoolId: userPoolId,
+      Username: username,
+    })
+    .promise();
+
+  console.log(`[${email}] - confirmed sign up`);
+
+  return {
+    username,
+    name,
+    email,
+  };
+};
+
 module.exports = {
   we_invoke_confirmUserSignup,
+  a_user_signs_up,
 };
